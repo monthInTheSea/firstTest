0e7f90f21e4471ec8df1beed7b08e5eef4304e3a
‘liyan’ <1327387522@qq.com>
2021-04-19 17:06:46 +0800

    企业微信-部门标签

---
+++ test_label.py
@@ -0,0 +1,40 @@
+import requests
+
+
+class TestLabel:
+
+    def setup(self):
+        url = " https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+        access_token = {
+                "corpid": "ww21b61721c9ef5272",
+                "corpsecret": "dHcc2iAcvnwqT1vxhYfnGWELLEG8Cw_3PEzQUZp-duc"
+            }
+
+        req = requests.get(url=url, params=access_token)
+        self.token = req.json()['access_token']
+
+    def test_create_label(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/tag/create?access_token={self.token}"
+        data ={
+                "tagname": "label",
+                "tagid": 12
+            }
+        req = requests.post(url=url, json=data)
+        assert req.json()['errcode'] == 0
+
+    def test_update_label_name(self):
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/tag/update?access_token={self.token}"
+        data = {
+             "tagid": 12,
+             "tagname": "放假提醒"
+        }
+        req = requests.post(url=url, json=data)
+        assert req.json()['errcode'] == 0
+
+    def test_delete_label(self):
+        tad_id = 12
+        url = f"https://qyapi.weixin.qq.com/cgi-bin/tag/delete?access_token={self.token}&tagid={tad_id}"
+        req = requests.get(url=url)
+        assert req.json()['errcode'] == 0
+
+    
